602aed9ba38d795addf96ed931035af59daa19e5
DevanK20000 <devanKhandagale@gmail.com>
2024-09-04 07:30:47 +0530

    added validation in entity

--- src/main/java/com/aurionpro/bankRest/entity/Customer.java
+++ src/main/java/com/aurionpro/bankRest/entity/Customer.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,11 +24,13 @@ public class Customer {
 
     @NotNull
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z]{2,16}$")
     @Column
     private String firstName;
 
     @NotNull
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z]{2,16}$")
     @Column
     private String lastName;
 
--- src/main/java/com/aurionpro/bankRest/entity/User.java
+++ src/main/java/com/aurionpro/bankRest/entity/User.java
@@ -1,6 +1,9 @@
 package com.aurionpro.bankRest.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,10 +20,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private int userId;
+    @NotNull
+    @NotBlank
     @Column
+    @Pattern(regexp = "^[a-zA-Z]{4,16}$", message = "Username should be 4 to 16 character long and should be alphabetic characters")
     private String username;
+
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W])[a-zA-Z\\d\\W]{3,16}$\n",
+            message = """
+                    At least one lowercase letter.
+                    At least one uppercase letter.
+                    At least one digit.
+                    At least one special character.
+                    The total length of the password is between 3 and 16 characters.""")
     @Column
     private String password;
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(joinColumns = @JoinColumn(name = "userId"), inverseJoinColumns = @JoinColumn(name = "roleId"))
     private List<Role> roles;
